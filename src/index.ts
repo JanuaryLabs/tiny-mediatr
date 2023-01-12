9e3447100e0849b1c21e16511ce4627301e66dad
ezzabuzaid <Ezzabuzaid@hotmail.com>
2023-01-12 23:07:24 +0300

    chore: cleanup comments

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
 export * from "./mediator";
 export * from "./notification";
 export * from "./request";
-
-// interface VM {}
-// class ListQuery extends IRequest<VM> {}
-
-// const app = express();
-
-// app.use((req, res, next) => {
-// 	req.mediator = new Mediator();
-// });
-
-// app.get("/", (req, res) => {
-// 	req.mediator.send(new ListQuery());
-// 	res.json([]);
-// });
-
-// app.listen();
-
-// declare global {
-// 	namespace Express {
-// 		export interface Request {
-// 			mediator: Mediator;
-// 		}
-// 	}
-// }
--- src/pipeline-behavior.ts
+++ src/pipeline-behavior.ts
@@ -1,10 +1,3 @@
-/// <summary>
-/// Represents an async continuation for the next task to execute in the pipeline
-/// </summary>
-/// <typeparam name="TResponse">Response type</typeparam>
-/// <returns>Awaitable task returning a <typeparamref name="TResponse"/></returns>
-// public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
-
 import { IRequest } from "./request";
 export type RequestHandlerDelegate<TResponse> = () => Promise<TResponse>;
 
